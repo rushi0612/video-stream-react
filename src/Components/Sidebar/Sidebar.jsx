10fe1ee424359d4e70c61189608090dbdf0bbed9
Rushikesh <rvp6123@gmail.com>
2026-02-20 18:13:48 +0530

    feat: add Sidebar component and integrate routing for Home and Video pages

---
+++ src/Components/Sidebar/Sidebar.jsx
@@ -0,0 +1,57 @@
+import React from 'react'
+import './Sidebar.css'
+import home from '../../assets/home.png'
+import game_icon from '../../assets/game_icon.png'
+import sports from '../../assets/sports.png'
+import automobile from '../../assets/automobiles.png'
+import entertainment from '../../assets/entertainment.png'
+import tech from '../../assets/tech.png'
+import music from '../../assets/music.png'
+import blogs from '../../assets/blogs.png'
+import news from '../../assets/news.png'
+import jack from '../../assets/jack.png'
+import simon from '../../assets/simon.png'
+import tom from '../../assets/tom.png'
+import megan from '../../assets/megan.png'
+import cameron from '../../assets/cameron.png'
+
+
+const Sidebar = () => {
+  return (
+    <div className="sidbar">
+        <div className="shortcut-links">
+            <div className="side-links">
+                <img src={home} alt="" /><p>Home</p>
+            </div>
+            <div className="side-links">
+                <img src={game_icon} alt="" /><p>Gaming</p>
+            </div>
+            <div className="side-links">
+                <img src={automobile} alt="" /><p>Automobile</p>
+            </div>
+            <div className="side-links">
+                <img src={sports} alt="" /><p>Sports</p>
+            </div>
+            <div className="side-links">
+                <img src={entertainment} alt="" /><p>Entertainment</p>
+            </div>
+            <div className="side-links">
+                <img src={home} alt="" /><p>Home</p>
+            </div>
+            <div className="side-links">
+                <img src={music} alt="" /><p>Music</p>
+            </div>
+            <div className="side-links">
+                <img src={blogs} alt="" /><p>Blogs</p>
+            </div>
+            <div className="side-links">
+                <img src={news} alt="" /><p>News</p>
+            </div>
+            
+            <hr />
+        </div>
+    </div>
+  )
+}
+
+export default Sidebar
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import './Home.css'
+import Sidebar from "../../Components/Sidebar/Sidebar"
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+      <Sidebar />
+    </>
   )
 }
 
