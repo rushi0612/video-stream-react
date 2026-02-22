8e70e614744c302a2fb94a5a7e2ca7dc1341af79
Rushikesh <rvp6123@gmail.com>
2026-02-22 22:38:40 +0530

    feat: add Feed component with styling and integrate into Home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import './Home.css'
 import Sidebar from "../../Components/Sidebar/Sidebar"
+import Feed from "../../Components/Feed/Feed"
 const Home = ({sidebar}) => {
   return (
     <>
       <Sidebar sidebar={sidebar} />
+      <div className={`container ${sidebar ? '' : 'large-container'}`}>
+          <Feed />
+      </div>
     </>
   )
 }
