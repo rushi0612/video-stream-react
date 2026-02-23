fd258c9ea9a1405a2d3a6cbc2a82cd1d85fc41cb
Rushikesh <rvp6123@gmail.com>
2026-02-23 22:43:59 +0530

    feat: enhance PlayVideo component with video details and interaction icons

--- src/Components/PlayVideo/PlayVideo.jsx
+++ src/Components/PlayVideo/PlayVideo.jsx
@@ -13,6 +13,16 @@ const PlayVideo = () => {
   return (
     <div className="play-video">
         <video src={video1} controls autoPlay muted></video>
+        <h3>Best YouTube Channel To Learn Coding</h3>
+        <div className="play-video-info">
+            <p>16k views &bull; 10 days ago</p>
+            <div>
+            <span><img src={like} alt="" />186</span>
+            <span><img src={dislike} alt="" />6</span>
+            <span><img src={share} alt="" />75</span>
+            <span><img src={save} alt="" />250</span>
+            </div>
+        </div>
     </div>
   )
 }
