c678593f868c25d81e61b2335c418227063ba3a6
Rushikesh <rvp6123@gmail.com>
2026-02-25 22:22:03 +0530

    feat: refactor Recommended component to include multiple thumbnails and improve layout

--- src/Components/Recommended/Recommended.jsx
+++ src/Components/Recommended/Recommended.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import './Recommended.css'
+import thumbnail1 from '../../assets/thumbnail1.png'
+import thumbnail2 from '../../assets/thumbnail2.png'
+import thumbnail3 from '../../assets/thumbnail3.png'
+import thumbnail4 from '../../assets/thumbnail4.png'
+import thumbnail5 from '../../assets/thumbnail5.png'
+import thumbnail6 from '../../assets/thumbnail6.png'
+import thumbnail7 from '../../assets/thumbnail7.png'
+import thumbnail8 from '../../assets/thumbnail8.png'
 
 const Recommended = () => {
   return (
-    <div>
-
+    <div className="recommended">
+        <div className="side-video-list">
+          <img src={thumbnail1} alt="Thumbnail 1" />
+          <div className="vid-info">
+            <h4>Best channel that help you to be a web developer</h4>
+            <p>RushiPatil</p>
+            <p>199k View</p>
+          </div>
+        </div>
     </div>
   )
 }
